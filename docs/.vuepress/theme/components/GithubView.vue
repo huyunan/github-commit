2626c7bd124d4d8b1ec7e4c46e0cf8035b4e8d33
huyunan <18842920976@163.com>
2025-09-14 21:17:16 +0800

    GithubView.vue 修改虽然在iframe里点击时还会有问题，以后再改

--- docs/.vuepress/theme/components/GithubView.vue
+++ docs/.vuepress/theme/components/GithubView.vue
@@ -1,31 +1,61 @@
 <script setup lang="ts">
-defineProps<{
+import { onMounted, ref } from 'vue';
+
+const props = defineProps<{
   src?: string
 }>()
 
+let flag = true
 function onload() {
+  console.log(11111111111)
   const iframe = document.getElementById('gitIframe');
   const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+  iframeDoc.addEventListener('click', function() {
+    console.log('Iframe hash changed');
+    flag = true
+    onload()
+  });
   const partialheader = iframeDoc.getElementById("partial-discussion-header")
   if (!partialheader) {
-    onload()
+    if (!flag) return
+    setTimeout(() => {
+      flag = false
+      onload()
+    }, 10000);
   } else {
-    partialheader.style.top = '-300px'
+    partialheader.style.display = 'none'
     const headerwrapper = iframeDoc.getElementsByClassName('header-wrapper')
-    if (headerwrapper && headerwrapper[0] && headerwrapper[0].style.display !== 'none') {
+    if (headerwrapper && headerwrapper[0]) {
       headerwrapper[0].style.display = 'none'
     }
     const stickyheader = iframeDoc.getElementById("sticky-header-backdrop")
-    if (stickyheader && stickyheader.style.display !== 'none') {
+    if (stickyheader) {
       stickyheader.style.display = 'none'
+    }
+    const footer = iframeDoc.getElementsByTagName('footer')
+    if (footer && footer[0]) {
+      footer[0].style.display = 'none'
+    }
+    const sidebar = iframeDoc.getElementsByClassName('Layout-sidebar')
+    if (sidebar && sidebar[0]) {
+      sidebar[0].style.display = 'none'
+    }
+    const timeline = iframeDoc.getElementsByClassName('discussion-timeline-actions')
+    if (timeline && timeline[0]) {
+      timeline[0].style.display = 'none'
     }
   }
 }
+
+let httpSrc = ref(props.src)
+onMounted(() => {
+  httpSrc.value = window.location.origin + props.src 
+})
 </script>
 
 <template>
   <div class="github-view">
-    <iframe :src="src"  sandbox="allow-scripts allow-same-origin" @load="onload" frameborder="0" id="gitIframe"></iframe>
+    <iframe :src="httpSrc" sandbox="allow-scripts allow-same-origin" @change="onload" @load="onload" frameborder="0" id="gitIframe"></iframe>
   </div>
 </template>
 
